4fec64373cccec109ae6b0d06be438a715cd1c18
Grigori Goronzy <greg@blackbox>
2013-01-28 06:43:09 +0100

    Use RGB565 surface for wallpaper
    The framebuffer only uses 16bpp on most legacy devices anyway, so this
    does not result in worse quality. It saves a lot of GPU fillrate and
    memory, though.
    
    Change-Id: If19902500d615291c4362f03d26d9cee2d4e684e

--- core/java/android/app/WallpaperManager.java
+++ core/java/android/app/WallpaperManager.java
@@ -364,7 +364,7 @@ public class WallpaperManager {
         Bitmap bm = sGlobals.peekWallpaperBitmap(mContext, true);
         if (bm != null) {
             Drawable dr = new BitmapDrawable(mContext.getResources(), bm);
-            dr.setDither(false);
+            dr.setDither(true);
             return dr;
         }
         return null;
@@ -383,7 +383,7 @@ public class WallpaperManager {
         Bitmap bm = sGlobals.peekWallpaperBitmap(mContext, false);
         if (bm != null) {
             Drawable dr = new BitmapDrawable(mContext.getResources(), bm);
-            dr.setDither(false);
+            dr.setDither(true);
             return dr;
         }
         return null;
