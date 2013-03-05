707d5a8cd04e485b4f487bde425f8ab53faf2183
Konsta <konsta09@gmail.com>
2013-03-05 15:15:29 +0200

    add property to disable hw acceleration on lockscreen
    Change-Id: I8ca5ac249e3211cf01d2528fee0f4a02d02d24d7

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
@@ -320,14 +320,18 @@ public class KeyguardViewManager {
 
             mKeyguardHost = new ViewManagerHost(mContext);
 
+            int hwAccelerated = android.os.SystemProperties.getInt("ro.hwaccel.lockscreen", 1);
+
             int flags = WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
                     | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR
                     | WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN
-                    | WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER
-                    | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
+                    | WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
 
             if (!mNeedsInput) {
                 flags |= WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM;
+            }
+            if (hwAccelerated != 0) {
+                flags |= WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
             }
 
             final int stretch = ViewGroup.LayoutParams.MATCH_PARENT;
@@ -337,8 +341,10 @@ public class KeyguardViewManager {
                     stretch, stretch, type, flags, PixelFormat.TRANSLUCENT);
             lp.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE;
             lp.windowAnimations = com.android.internal.R.style.Animation_LockScreen;
-            lp.flags |= WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
-            lp.privateFlags |= WindowManager.LayoutParams.PRIVATE_FLAG_FORCE_HARDWARE_ACCELERATED;
+            if (hwAccelerated != 0) {
+                lp.flags |= WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
+                lp.privateFlags |= WindowManager.LayoutParams.PRIVATE_FLAG_FORCE_HARDWARE_ACCELERATED;
+            }
             lp.privateFlags |= WindowManager.LayoutParams.PRIVATE_FLAG_SET_NEEDS_MENU_KEY;
             if (isActivity) {
                 lp.privateFlags |= WindowManager.LayoutParams.PRIVATE_FLAG_SHOW_FOR_ALL_USERS;
